7d28aa1abe5bee4ab0134f77e9942e8e81bd5b7b
Matheus Souza <mathsouzadevs@gmail.com>
2025-08-25 16:42:48 -0300

    Adicionando o Angular Material ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PacotesComponent } from './pacotes/pacotes.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormularioComponent } from './formulario/formulario.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
