332c00ece8cdf4a4d80fad66b6fdda6a97160adb
Matheus Souza <mathsouzadevs@gmail.com>
2025-08-25 16:37:36 -0300

    Adicionando os componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PacotesComponent } from './pacotes/pacotes.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PacotesComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
