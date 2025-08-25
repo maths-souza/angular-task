c90e16d63de10b086062032d51f1359610513404
Matheus Souza <mathsouzadevs@gmail.com>
2025-08-25 18:02:00 -0300

    Adicionando o botão do Pacote

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PacotesComponent } from './pacotes/pacotes.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PacotesComponent},
+  {path: 'pacote', component: PacotesComponent},
+  {path: 'formulario', component: FormularioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
